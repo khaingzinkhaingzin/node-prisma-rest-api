b221574dd875ce1130fed8c6814ff6f4ebebd9e8
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2025-03-11 12:03:00 +0630

    [FINISH] get categories endpoint GET /categories and update categories endpoint PUT /categories:id

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -22,3 +22,45 @@ exports.createCategory = async (req, res) => {
         return res.status(500).json({ error: error.message })
     }
 }
+
+exports.getCategories = async (req, res) => {
+    try {
+        const categories = await prisma.category.findMany()
+
+        return res.status(200).json(categories)
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+exports.updateCategory = async (req, res) => {
+    try {
+        if (!await prisma.category.findUnique({ where: { id: parseInt(req.params.id) } })) {
+            return res.status(404).json({ error: 'Category not found' })
+        }
+
+        if (!req.body.name) {
+            return res.status(422).json({ error: 'Name is required' })
+        }
+
+        if (await prisma.category.count({
+            where: {
+                AND: [
+                    { id: { not: parseInt(req.params.id) } },
+                    { name: req.body.name }
+                ]
+            }
+        })) {
+            return res.status(409).json({ error: `${req.body.name} category already exists` })
+        }
+
+        const updateCategory = await prisma.category.update({
+            data: { name: req.body.name },
+            where: { id: parseInt(req.params.id) }
+        });
+
+        return res.status(200).json(updateCategory)
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
